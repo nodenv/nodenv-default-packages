684615a22a1e3923a940202c59c16459ade53e58
Jason Karns <jason@karns.name>
2019-08-13 10:15:47 -0400

    common nodenv test setup

--- test/test_helper.bash
+++ test/test_helper.bash
@@ -5,16 +5,32 @@ load ../node_modules/bats-assert/load
 load ../node_modules/bats-support/load
 load ../node_modules/bats-mock/stub
 
-NODENV_TEST_DIR="${BATS_TMPDIR}/nodenv"
-mkdir -p "${NODENV_TEST_DIR}"
+setup() {
+  # common nodenv setup
 
-export NODENV_ROOT="${NODENV_TEST_DIR}"
+  # unset all NODENV_ vars
+  for nodenv_var in $(env 2>/dev/null | grep '^NODENV_' | cut -d= -f1); do
+    unset "$nodenv_var"
+  done
+  unset nodenv_var
 
-PATH=/usr/bin:/bin:/usr/sbin:/sbin
-PATH="$BATS_TEST_DIRNAME/helpers/bin:$PATH"
-PATH="$BATS_TEST_DIRNAME/../bin:$PATH"
-PATH="$BATS_MOCK_BINDIR:$PATH"
-export PATH
+  # set a restricted PATH (test bin, app bin, and node bin, no homebrew)
+  local test_bin="$BATS_TEST_DIRNAME/bin"
+  local package_bin="$BATS_TEST_DIRNAME/../bin"
+  local node_modules_bin="$BATS_TEST_DIRNAME/../node_modules/.bin"
+  local core_path="/usr/bin:/bin:/usr/sbin:/sbin"
+
+  export PATH="$BATS_MOCK_BINDIR:$test_bin:$package_bin:$node_modules_bin:$core_path"
+  unset test_bin package_bin node_modules_bin core_path
+
+
+
+  # custom setup
+  NODENV_TEST_DIR="${BATS_TMPDIR}/nodenv"
+  mkdir -p "${NODENV_TEST_DIR}"
+
+  export NODENV_ROOT="${NODENV_TEST_DIR}"
+}
 
 teardown() {
   rm -rf "$NODENV_TEST_DIR"
