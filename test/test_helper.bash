d180fb04680d20c9844f9f82e66e5abcb38d047f
Jason Karns <jason@karns.name>
2019-08-13 10:38:39 -0400

    Don't need to unset locals

--- test/test_helper.bash
+++ test/test_helper.bash
@@ -8,19 +8,17 @@ setup() {
   # common nodenv setup
 
   # unset all NODENV_ vars
+  local nodenv_var
   for nodenv_var in $(env 2>/dev/null | grep '^NODENV_' | cut -d= -f1); do
     unset "$nodenv_var"
   done
-  unset nodenv_var
 
   # set a restricted PATH (test bin, app bin, and node bin, no homebrew)
   local test_bin="$BATS_TEST_DIRNAME/bin"
   local package_bin="$BATS_TEST_DIRNAME/../bin"
   local node_modules_bin="$BATS_TEST_DIRNAME/../node_modules/.bin"
   local core_path="/usr/bin:/bin:/usr/sbin:/sbin"
-
   export PATH="$BATS_MOCK_BINDIR:$test_bin:$package_bin:$node_modules_bin:$core_path"
-  unset test_bin package_bin node_modules_bin core_path
 
 
 
